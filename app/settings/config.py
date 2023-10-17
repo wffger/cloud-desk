8beec72676032b1a54d79599ede3bb0ab9d4b64c
wffger <wffger@163.com>
2023-10-17 16:09:32 +0800

    if secrects.toml not exists, use env vars

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,10 +1,17 @@
 import logging
+import os
 from dynaconf import Dynaconf
 from fastapi.templating import Jinja2Templates
 
 env = Dynaconf(
   settings_files=['settings/conts.toml', 'settings/.secrets.toml']
   )
+
+if not os.path.exists('settings/.secrets.toml'):
+    env.AWS_REGION_NAME = os.getenv('AWS_REGION_NAME', 'ap-southeast-1')
+    env.AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+    env.AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
+    env.AWS_S3_BUCKET = os.getenv('AWS_S3_BUCKET', '')
 
 # Set up our logger
 match env.LOG_LEVEL.upper():
